a3e248afe87914af435d7d2c3a9bd204cb810a50
Miguel Benavente Valdes <benavente.miguel@gmail.com>
2016-04-19 12:23:32 -0500

    Initial Commit

---
+++ WatchPizza WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchPizza WatchKit Extension
+//
+//  Created by Miguel Benavente Valdés on 19/04/16.
+//  Copyright © 2016 Miguel Benavente Valdés. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ WatchPizza/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchPizza
+//
+//  Created by Miguel Benavente Valdés on 19/04/16.
+//  Copyright © 2016 Miguel Benavente Valdés. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
